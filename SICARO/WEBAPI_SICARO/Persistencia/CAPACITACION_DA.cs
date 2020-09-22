9a4362f15f3ff148084472ab8581aa5347b9285a
raulp07 <sefiroth1990@gmail.com>
2020-09-22 09:04:21 -0500

    Correos Capacitacion

--- SICARO/WEBAPI_SICARO/Persistencia/CAPACITACION_DA.cs
+++ SICARO/WEBAPI_SICARO/Persistencia/CAPACITACION_DA.cs
@@ -251,7 +251,65 @@ namespace WEBAPI_SICARO.Persistencia
 
         public int FinalizarCapacitacion(int idcapacitacion)
         {
-            return 1;
+            using (SqlConnection con = new SqlConnection(ConexionUtil.Cadena))
+            {
+                con.Open();
+                using (SqlCommand com = new SqlCommand("FinalizarCapacitacion", con))
+                {
+                    com.CommandType = CommandType.StoredProcedure;
+                    com.Parameters.Add("@iIdCapacitacion", SqlDbType.Int).Value = idcapacitacion;
+                    
+                    using (IDataReader dataReader = com.ExecuteReader())
+                    {
+                        string CorreosOpererariosFaltantes = "";
+                        string ContenidoOperarioFaltantes = "";
+                        string temaCapacitacion = "";
+
+                        int Aprobado = 0;
+                        int Desaprobado = 0;
+                        int NroParticipante = 0;
+                        string ContenidoFinalizacionCapacitacion = "";
+
+                        while (dataReader.Read())
+                        {
+                            if (dataReader["Email"] != DBNull.Value) { CorreosOpererariosFaltantes += (string)dataReader["Email"] + ";"; }
+                        }
+
+                        while (dataReader.NextResult())
+                        {
+                            while (dataReader.Read())
+                            {
+                                if (dataReader["Aprobado"] != DBNull.Value) { Aprobado = (int)dataReader["Aprobado"]; }
+                                if (dataReader["Desaprobado"] != DBNull.Value) { Desaprobado = (int)dataReader["Desaprobado"]; }
+                                if (dataReader["NroParticipante"] != DBNull.Value) { NroParticipante = (int)dataReader["NroParticipante"]; }
+                                if (dataReader["TemaCapacitacion"] != DBNull.Value) { temaCapacitacion = (string)dataReader["TemaCapacitacion"]; }
+                            }
+                        }
+
+                        // Envio de correo a Personal que no asistieron a la capacitacion
+                        ContenidoOperarioFaltantes = "Estimado Colaborador: <br /><br />" +
+                                                     "Este correo es para hacerle saber que WISLAN S.A.C. no ha podido contar con su participación para la capacitación : " + temaCapacitacion + ".<br />" +
+                                                     "Los organizadores del evento se encuentran interesados en su instrucción continua y desean saber el motivo por el cual no pudo asistir a la capacitación, por favor presentar su descargo a la siguiente dirección electrónica : sys.icaro@gmail.com <br /><br />" +
+                                                     "Gracias de antemano por su respuesta <br /><br />" +
+                                                     "Saludos.";
+                        Utilitarios.Accion.EnvioCorreo(CorreosOpererariosFaltantes, "Falta a la capacitación  ", ContenidoOperarioFaltantes);
+
+                        //Envio el resultado final de la capacitacion
+                        ContenidoFinalizacionCapacitacion = "Estimado Jefe de Aseguramiento de la calidad: <br /><br />" +
+                                                            "Este correo es para informarle que habiendo concluido la capacitación con el tema " + temaCapacitacion + " que contó con la participación de" + NroParticipante + "miembros de nuestra organización. <br /><br />"+
+                                                            "Se tuvieron en los resultados del test de comprobación de conocimientos: <br/ >" +
+                                                            "- " + Aprobado + " Aprobados <br />" +
+                                                            "- " + Desaprobado + " Desaprobados <br /><br />" +
+                                                            "Agradecemos que pueda conservar el resultado o informar a las personas correspondientes.<br /><br />" +
+                                                            "Saludos";
+                        Utilitarios.Accion.EnvioCorreo("jefe.calidad.wislan@gmail.com", "Capacitación Finalizada ", ContenidoFinalizacionCapacitacion);
+
+                    }
+                    return 1;
+                }
+
+            }
+
             //using (SqlConnection con = new SqlConnection(ConexionUtil.Cadena))
             //{
             //    con.Open();
---
+++ SICARO/WEBAPI_SICARO/Utilitarios.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+
+namespace WEBAPI_SICARO
+{
+    public class Utilitarios
+    {
+        private static Utilitarios accion;
+        private Utilitarios() { }
+        public static Utilitarios Accion
+        {
+            get
+            {
+                if (accion == null)
+                {
+                    accion = new Utilitarios();
+                }
+                return accion;
+            }
+        }
+        public string EnvioCorreo(string destinatario, string titulo, string Mensaje, string Archivo = "")
+        {
+            string MSG = "";
+            try
+            {
+                var listaCorreos = destinatario.Split(';');
+
+                MailMessage correo = new MailMessage();
+                correo.From = new MailAddress("Sys.ICARO@gmail.com");
+
+                foreach (string item in listaCorreos)
+                {
+                    if (item != "")
+                    {
+                        correo.To.Add(item);
+                    }
+                }
+
+                correo.Subject = titulo;
+                correo.Body = Mensaje;
+                correo.IsBodyHtml = true;
+                correo.Priority = MailPriority.Normal;
+                if (Archivo != "")
+                {
+                    string rutaArchivo = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, Archivo);
+                    if (File.Exists(rutaArchivo))
+                    {
+                        Attachment a = new Attachment(rutaArchivo);
+                        correo.Attachments.Add(a);
+                        //File.Delete(rutaArchivo);
+                    }
+                }
+
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 25;
+                smtp.EnableSsl = true;
+                smtp.UseDefaultCredentials = true;
+                string cuentacorreo = "Sys.ICARO@gmail.com";
+                string password = "webicar0";
+                smtp.Credentials = new System.Net.NetworkCredential(cuentacorreo, password);
+
+                smtp.Send(correo);
+                MSG = "Mensaje Enviado";
+            }
+            catch (Exception e)
+            {
+                MSG = e.Message;
+            }
+            return MSG;
+
+        }
+    }
+}
